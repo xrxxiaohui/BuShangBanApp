d525dd826a428892aad51190751bbb1d5539faae
wangluting <wangluting@bushangban.com>
2016-04-26 20:05:07 +0800

    完成用户隐私页面

---
+++ BuShangBanApp/BuShangBanApp/Classes/RightController/我的/Setting/UsingSetting/UserProtocalViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserProtocalViewController.h
+//  BuShangBanApp
+//
+//  Created by mac on 16/4/26.
+//  Copyright © 2016年 Zuo. All rights reserved.
+//
+
+#import "QHBasicViewController.h"
+
+@interface UserProtocalViewController : QHBasicViewController
+
+@end
